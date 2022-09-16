d1494fc324667e498dc25229d8c3af5a5235b826
Jayashanka Deshan <maxwon555@gmail.com>
2022-09-16 22:00:24 +0530

    clean driver code and call new functions to it

---
+++ src/driver_code.java
@@ -0,0 +1,10 @@
+
+import Encoding.*;
+
+public class driver_code
+{
+    public static void main(String[] args)
+    {
+        System.out.print("Text We are going to Encryption: "+Encoding_Data.Encoding_data("aesEncryptionKey", "password12345"));
+    }
+}
