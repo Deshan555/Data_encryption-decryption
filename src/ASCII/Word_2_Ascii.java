e9a085404a9296606fdc043d68aa264ded6c9356
Jayashanka Deshan <maxwon555@gmail.com>
2022-09-17 11:46:00 +0530

    that new function convert ascii values into related string sentence

--- src/ASCII/Word_2_Ascii.java
+++ src/ASCII/Word_2_Ascii.java
@@ -1,4 +1,24 @@
 package ASCII;
 
-public class Word_2_Ascii {
+public class Word_2_Ascii
+{
+    public static String ASCIISentence(String str)
+    {
+        int l = str.length();
+
+        String ASCII_Encoded  = null;
+
+        int convert;
+
+        for (int i = 0; i < l; i++)
+        {
+            convert = str.charAt(i);
+
+            ASCII_Encoded = ASCII_Encoded + convert;
+        }
+
+        ASCII_Encoded = ASCII_Encoded.replace("null", "");
+
+        return ASCII_Encoded;
+    }
 }
